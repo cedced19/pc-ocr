8add63d5fd472343db8384c086a9a33bdf779bdf
Cédric JUNG <cedced19@gmail.com>
2017-05-06 17:35:42 +0200

    Save socket session in a couple pc/phone

--- sockets/index.js
+++ sockets/index.js
@@ -1,5 +1,38 @@
-module.exports = function (io) {
-  return function (socket) {
+var getObject = function(array, roomId, cb) {
+  for(var i in array) {
+    if (array[i].id == roomId) {
+      return cb(i);
+    }
+  }
+  cb(false);
+}
 
+
+module.exports = function (io) {
+  var rooms = [];
+  return function (socket) {
+    var user = {};
+    socket.on('login', function (data) {
+      user.roomId = data.id;
+      user.type = data.type;
+      socket.join(user.roomId);
+      getObject(rooms, user.roomId, function (result) {
+        if (result === false) {
+          var room = {id: user.roomId};
+          if (user.type == 'computer') {
+            room.computer = socket.id;
+          } else {
+            room.phone = socket.id;
+          }
+          rooms.push(room);
+        } else {
+          if (user.type == 'computer') {
+            rooms[result].computer = socket.id;
+          } else {
+            rooms[result].phone = socket.id;
+          }
+        }
+      });
+    });
   }
 }
